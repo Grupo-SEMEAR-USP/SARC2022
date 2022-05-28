9a657ce51ca6226a07e26258b4cad5bb9d759967
Matheus Della Rocca Martins <84210251+MatheusDrm@users.noreply.github.com>
2022-05-28 19:38:06 -0300

    Add function to find fire area centroid

--- python/uav/sensors/camera.py
+++ python/uav/sensors/camera.py
@@ -16,6 +16,7 @@ class uavCamera:
     red_trees_bgr =  [0, 0, 0]
     yellow_trees_bgr = [0, 0, 0]
     green_trees_bgr = [0, 0, 0]
+
     
     def __init__(   self,
                     img_width: int,
@@ -31,9 +32,9 @@ class uavCamera:
         self.sub = rospy.Subscriber(name = self.subscriber_name,
                                         data_class = Image,
                                         callback = self.img_msg_to_cv)
-        
         self.cv_img = np.zeros((img_height, img_width, 3), dtype= np.uint8)
-        self.cv_img_hsv = cv2.cvtColor(self.cv_img, cv2.COLOR_BGR2HSV)
+        self.blurred_img = cv2.GaussianBlur( self.cv_img, (5,5), 0)
+        self.cv_img_hsv = cv2.cvtColor(self.blurred_img, cv2.COLOR_BGR2HSV)
         self.i_see_fire: bool = False
 
         #Init camera node
@@ -54,7 +55,7 @@ class uavCamera:
         '''
             img_msg: image input from ROS
         '''
-        #Subscribe to camera in Gazebo and apply img_msg_to_cv() as callback function
+        #  to camera in Gazebo and apply img_msg_to_cv() as callback function
         # self.img_msg = rospy.Subscriber(name = self.subscriber_name,
         #                                 data_class = Image,
         #                                 callback = self.img_msg_to_cv)
@@ -64,24 +65,47 @@ class uavCamera:
         
         # self.cv_img_hsv = cv2.cvtColor(self.cv_img, cv2.COLOR_BGR2HSV)
         
-
     def color_detection(    self,
                             img_src: np.array,
                             lower_values: list,
                             upper_values: list) -> None:
         
-        __img = np.copy(img_src)
-
-        self.mask = cv2.inRange(    __img,
-                                    lower_values,
-                                    upper_values)
-
-        self.cv_img_masked = cv2.bitwise_and(   __img,
-                                                __img,
-                                                mask=self.mask)                    
+        self.img = np.copy(img_src)
         
+        self.blurred_img = cv2.GaussianBlur( self.img, (5,5), 0)
+
+        self.cv_img_hsv = cv2.cvtColor(self.blurred_img, cv2.COLOR_BGR2HSV)
+
+        self.mask = cv2.inRange( self.cv_img_hsv, lower_values, upper_values)
+
+        self.cv_img_masked = cv2.bitwise_and( self.img, self.img, mask=self.mask)
+
+                
+    # Centroid calculations
+    def find_centroid(    self
+                            ) -> None:
+                            
+        countours, hierarchy= cv2.findContours(self.mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        cv2.drawContours( self.mask, countours, 0, (0,255,0), 3)
+        for countour in countours: 
+            self.area = cv2.contourArea(countour)
+            #print(self.area)
+            if self.area>10000: 
+                self.moment = cv2.moments(countour)
+                # position of the centroid
+                self.cx = int(self.moment["m10"]/self.moment["m00"]) 
+                self.cy = int(self.moment["m01"]/self.moment["m00"])
+                #  Creating a point to represent the centroid 
+                cv2.circle( self.img, (self.cx, self.cy), 7, (255, 255, 255), -1)
+
+    
+
+
+
+
     def display_img(self):
         cv2.imshow("uav View", self.cv_img)
+        
         
         
 
@@ -89,9 +113,6 @@ class uavCamera:
         self.sub = rospy.Subscriber(name = self.subscriber_name,
                                         data_class = Image,
                                         callback = self.img_msg_to_cv)
-
-        
-
         
         
         
